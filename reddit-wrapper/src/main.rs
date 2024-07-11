a320872a702c64c4c6b914517269bf8b920c1b01
Jean-Alain Morel <81867817+Jay-boo@users.noreply.github.com>
2024-07-11 16:28:44 +0200

    Unitest implementation for redditWrapper (#88)
    [Rust] Unittest on reddit wrapper stream

--- reddit-wrapper/src/main.rs
+++ reddit-wrapper/src/main.rs
@@ -43,7 +43,6 @@ async fn main()-> Result<(),std::io::Error> {
         .set("request.timeout.ms","1000")
         .create().unwrap();
     //Documentation available here :https://github.com/confluentinc/librdkafka/blob/master/CONFIGURATION.md
-    let stream1 = futures::stream::iter(vec![1, 2, 3]);
 
 
 
@@ -55,9 +54,9 @@ async fn main()-> Result<(),std::io::Error> {
     let me:me::me::Me=reddit_client.login(&USER_NAME, &PASSWORD).await.unwrap();
     info!("{}",format!("Get subreddit: {} ","r/funny"));
     let rfunny:subreddit::subreddit::Subreddit=me.get_subreddit("r/funny",Some(1),subreddit::feedoptions::FeedFilter::Hot).await;
-    let (stream_posts_rfunny,join_handle)= rfunny.stream_items(Duration::new(10, 0),"Nothing".to_string(),None);
+    let (stream_posts_rfunny,join_handle)= rfunny.stream_items(Duration::new(10, 0),"Nothing".to_string(),None,None);
     let rrust:subreddit::subreddit::Subreddit=me.get_subreddit("r/rust",Some(1),subreddit::feedoptions::FeedFilter::Hot).await;
-    let (stream_posts_rrust,join_handle)= rrust.stream_items(Duration::new(10, 0),"Nothing".to_string(),None);
+    let (stream_posts_rrust,join_handle)= rrust.stream_items(Duration::new(10, 0),"Nothing".to_string(),None,None);
 
 
     let mut combined_stream=futures::stream::select_all(vec![stream_posts_rrust,stream_posts_rfunny]);
@@ -96,45 +95,4 @@ async fn main()-> Result<(),std::io::Error> {
     Ok(())
 
 }
-
-#[cfg(test)]
-mod tests{
-
-    use core::panic;
-    use dotenv::dotenv;
-    use reqwest::{Client, Response, header::USER_AGENT};
-
-    lazy_static::lazy_static!{
-        static ref USER_AGENT_NAME:String=std::env::var("USER_AGENT_NAME").expect("USER_AGENT_NAME not set");
-        static ref CLIENT_ID:String=std::env::var("CLIENT_ID").expect("CLIENT_ID not set");
-        static ref CLIENT_SECRET:String=std::env::var("CLIENT_SECRET").expect("CLIENT_SECRET not set");
-        static ref USER_NAME:String=std::env::var("USER_NAME").expect("USER_NAME not set");
-        static ref PASSWORD:String=std::env::var("PASSWORD").expect("PASSWORD not set");
-    }
-
-    #[tokio::test]
-    async fn test_auhentication(){
-        println!("Authentication test");
-        dotenv().ok();
-        let url:&str="https://www.reddit.com/api/v1/access_token";
-        let form = [
-                ("grant_type", "password"),
-                (
-                    "username",&USER_NAME
-                ),
-                (
-                    "password",&PASSWORD
-                ),
-            ];
-        let response:Response=match Client::new()
-            .post(url).header(USER_AGENT,&*USER_AGENT_NAME)
-            .basic_auth(&*CLIENT_ID,Some(&*CLIENT_SECRET)).form(&form).send().await{
-                Ok(response)=>response,
-                Err(_e)=> panic!("{}",format!("Authentication request failed to {}  \nwith{:#?}!",url,form))
-            };
-        
-        println!("response:{:#?}",response.status())
-    }
-}
-
 
