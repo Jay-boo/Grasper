b2049fd3a5f7c5b8c934d35efdba0f9d4c069705
jay <jeanalainpro2@gmail.com>
2024-08-07 16:56:26 +0200

    [Rust - sof-wrapper] Streaming tags popularity

--- sof-wrapper/src/client/stackExchangeClient.rs
+++ sof-wrapper/src/client/stackExchangeClient.rs
@@ -1,21 +1,32 @@
+use std::time::Duration;
+
 use log::{debug, info, warn};
 use reqwest::{Client, Response};
 use crate::client::stackExchangeClientErrors::FetchError;
 use serde::Deserialize;
+use futures::{Stream, channel::mpsc, task::waker, Sink, SinkExt, TryFutureExt};
+use tokio::{task::JoinHandle, time::sleep};
 
+use super::stackExchangeClientErrors::StreamError;
+
+
+#[derive(Clone)]
 pub struct StackExchangeSimpleClient{
     client:Client,
     tags:Vec<SOFTag>
 }
 
-#[derive(Deserialize,Debug)]
+#[derive(Deserialize,Debug,Clone)]
 pub struct SOFTag{
-    name:String
+    name:String,
+    count:i32,
+    answers:Option<i32>
 }
 #[derive(Deserialize,Debug)]
 struct TagResponse{
     items:Vec<SOFTag>
 }
+
 
 impl StackExchangeSimpleClient{
     pub fn new()->Self{
@@ -31,7 +42,7 @@ impl StackExchangeSimpleClient{
 
     }
 
-    pub async fn fetch_all_tags(&mut self)->Result<(),FetchError>{
+    pub async fn fetch_all_tags(&self)->Result<Vec<SOFTag>,FetchError>{
         let mut tags:Vec<SOFTag>=vec![];
         for p in 1..26{
             match self.fetch_tag_page(p).await{
@@ -46,16 +57,43 @@ impl StackExchangeSimpleClient{
             return  Err(FetchError::new("No tags detected"));
         }
         debug!("{:?}",tags);
-        self.tags=tags;
-        Ok(())
+        Ok(tags)
+    }
+
+    async fn pull_into_sink<S:Sink<Result<Vec<SOFTag>,StreamError>>+Unpin>(&mut self,mut sink:S,sleep_time:Duration,timeout:Option<Duration>)->Result<(),S::Error>{
+        loop {
+            debug!("Fetching latest tag popularity");
+            let tags:Result<Vec<SOFTag>,StreamError>=match timeout{
+                Some(timeout_duration)=>{
+                    let timeout_result:Result<Result<Vec<SOFTag>, StreamError>, tokio::time::error::Elapsed>=tokio::time::timeout(
+                        timeout_duration, 
+                        self.fetch_all_tags().map_err(StreamError::FetchError)
+                    ).await;
+                    match timeout_result{
+                        Ok(fetch_result)=>fetch_result,
+                        Err(timeout_error)=>Err(StreamError::TimeoutError(timeout_error))
+                    }
+
+                },
+                None=>self.fetch_all_tags().await.map_err(StreamError::FetchError)
+            };
+            
+            sink.send(tags).await;
+            sleep(sleep_time).await;
+            
+        }
+    }
+
+    pub fn stream(&self,sleep_time:Duration,timeout:Option<Duration>)->(impl Stream<Item =Result<Vec<SOFTag>,StreamError>>,JoinHandle<Result<(),mpsc::SendError>>){
+        let (sender,stream)=mpsc::unbounded();
+        let mut owned_client=self.clone();
+        let join_handle =tokio::task::spawn(
+            async move{
+                owned_client.pull_into_sink(sender,sleep_time,timeout).await
+            }
+        );
+        (stream,join_handle)
     }
     
-    pub fn fetch_tag_popularity(&self,tag:&str)-> Result<(),FetchError>{
-        // let url= 
-        // self.client.get(url)
-        Ok(())
-
-
-    }
 }
 
